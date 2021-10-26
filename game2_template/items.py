29890f9268abb362fe6767c551c55f3f3728710a
claymore9000 <52158000+claymore9000@users.noreply.github.com>
2021-10-26 19:07:24 +0100

    Add files via upload

---
+++ game2_template/items.py
@@ -0,0 +1,83 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+    
+    "weight": 0.1,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "weight": 2,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "weight": 0.5,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "weight": 1,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "weight": 0.2,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "weight": 1.5,
+
+    "description": "This student handbook explains everything. Seriously."
+}
+
+item_brick = {
+    "id": "brick",
+
+    "name": "a brick",
+
+    "weight": 5,
+
+    "description": "It is literally just a brick, what were you expecting?"
+}
+
+items = {
+    "id": item_id,
+    "laptop": item_laptop,
+    "money": item_money,
+    "biscuits": item_biscuits,
+    "pen": item_pen,
+    "handbook": item_handbook,
+    "brick": item_brick
+}
